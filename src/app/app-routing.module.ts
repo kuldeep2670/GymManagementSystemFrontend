87d5f589978c9f207445c9c146f46699b044b846
Kuldeep Singh <kuldeepsingh2670@gmail.com>
2024-02-27 18:26:35 +0530

    Gym module with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GetAllGymAdminComponent } from './GymComponent/get-all-gym-admin/get-all-gym-admin.component';
-import { AddGymAdminComponent } from './GymComponent/add-gym-admin/add-gym-admin.component';
-import { GetGymAdminByIdComponent } from './GymComponent/get-gym-admin-by-id/get-gym-admin-by-id.component';
-import { DeleteGymAdminByIdComponent } from './GymComponent/delete-gym-admin-by-id/delete-gym-admin-by-id.component';
-import { UpdateGymAdminByIdComponent } from './GymComponent/update-gym-admin-by-id/update-gym-admin-by-id.component';
+import { GetAllGymAdminComponent } from './gym/get-all-gym-admin/get-all-gym-admin.component';
+import { AddGymAdminComponent } from './gym/add-gym-admin/add-gym-admin.component';
+import { GetGymAdminByIdComponent } from './gym/get-gym-admin-by-id/get-gym-admin-by-id.component';
+import { DeleteGymAdminByIdComponent } from './gym/delete-gym-admin-by-id/delete-gym-admin-by-id.component';
+import { UpdateGymAdminByIdComponent } from './gym/update-gym-admin-by-id/update-gym-admin-by-id.component';
+import { GymComponent } from './gym/gym.component';
 
 const routes: Routes = [
-  {path:"gym/getall", component:GetAllGymAdminComponent},
-  {path:"gym/add", component:AddGymAdminComponent},
-  {path:"gym/get", component:GetGymAdminByIdComponent},
-  {path:"gym/delete", component:DeleteGymAdminByIdComponent},
-  {path:"gym/update", component:UpdateGymAdminByIdComponent}
+  {
+    path:'gym',
+    component:GymComponent,
+    children:[
+      {path:'getall', component:GetAllGymAdminComponent},
+      {path:'add', component:AddGymAdminComponent},
+      {path:'get', component:GetGymAdminByIdComponent},
+      {path:'delete', component:DeleteGymAdminByIdComponent},
+      {path:'update', component:UpdateGymAdminByIdComponent},
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { GetAllGymAdminComponent } from './GymComponent/get-all-gym-admin/get-all-gym-admin.component';
+import { GetAllGymAdminComponent } from './gym/get-all-gym-admin/get-all-gym-admin.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AddGymAdminComponent } from './GymComponent/add-gym-admin/add-gym-admin.component';
-import { GetGymAdminByIdComponent } from './GymComponent/get-gym-admin-by-id/get-gym-admin-by-id.component';
-import { DeleteGymAdminByIdComponent } from './GymComponent/delete-gym-admin-by-id/delete-gym-admin-by-id.component';
-import { UpdateGymAdminByIdComponent } from './GymComponent/update-gym-admin-by-id/update-gym-admin-by-id.component';
+import { AddGymAdminComponent } from './gym/add-gym-admin/add-gym-admin.component';
+import { GetGymAdminByIdComponent } from './gym/get-gym-admin-by-id/get-gym-admin-by-id.component';
+import { DeleteGymAdminByIdComponent } from './gym/delete-gym-admin-by-id/delete-gym-admin-by-id.component';
+import { UpdateGymAdminByIdComponent } from './gym/update-gym-admin-by-id/update-gym-admin-by-id.component';
+import { GymComponent } from './gym/gym.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { UpdateGymAdminByIdComponent } from './GymComponent/update-gym-admin-by-
     AddGymAdminComponent,
     GetGymAdminByIdComponent,
     DeleteGymAdminByIdComponent,
-    UpdateGymAdminByIdComponent
+    UpdateGymAdminByIdComponent,
+    GymComponent
   ],
   imports: [
     BrowserModule,
